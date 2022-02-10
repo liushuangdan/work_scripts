55518fe7131c5e7b10142782b2fff24489ee39ba
liushuangdan <liushuangdan520@163.com>
2022-02-10 11:24:51 +0800

    add bing dundun
    add bing dundun

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,32 +1,51 @@
 
 import csv
 import json
-# fh = open(r"C:\Users\liushuangdan\Desktop\sfr_2021-08-06.csv", "w+")
-# writer = csv.writer(fh)
-# writer.writerow(["城市代码","城市名称"])
-data = open(r"C:\Users\liushuangdan\Desktop\sfr_2021-08-06.txt", encoding="utf_8_sig")
-res = []
-columns = ['reposts_count', 'posttime', 'site_name', 'vid', 'fetch_time', 'title', 'url', 'author', 'play_count', 'comments_count', 'author_url', 'author_followers_count', 'favourites_count', 'author_id', 'subdomain', 'author_statuses_coun', 'author_favourites_count', 'author_img', 'album_id', 'author_name', 'album', 'reposts_coun', 'platform', 'album_url', 'albums_count', 'play_time', 'author_info', 'album_subscribe_count', 'author_favorite_count', 'album_play_count', 'album_item_num', 'author_statuses_count', 'coin_count', 'collection_count']
-for i in data:
+import os
+columns = ['wx_nickname', 'wx_name', 'nickname_id', 'posttime', 'wx_title', 'url', 'add_time', 'readnum', 'likenum', 'old_like_num', 'top']
 
-    if i.endswith("\n"):
-        i = i.replace("\n", "").replace("\r", "")
-    d = json.loads(i)
-    temp_dict = {}
-    for item in columns:
-        temp_dict[item] = d.get(item, None)
-    # print(d.keys())
-    res.append(temp_dict)
-# print(res)
-csv_file = open(r"C:\Users\liushuangdan\Desktop\sfr_2021-08-06-new.csv", 'w', encoding="utf_8_sig", newline="")
-json_list = res
-json_values = []
-for one in json_list:
-    # print(one.keys())
-    # print('**'*30)
-    # print(len(one.values()), one.values())
-    json_values.append(one.values())
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(columns)
-csv_writer.writerows(json_values)
-csv_file.close()
+
+def eachFiles(filepath):
+    child_file = []
+    pathDir = os.listdir(filepath)
+    for allDir in pathDir:
+        child = os.path.join('%s\%s' % (filepath, allDir))
+        if os.path.isfile(child):
+            if child.endswith(".log"):
+                child_file.append(child)
+    return child_file
+
+
+def readFile(file):
+    data = open(file, encoding="utf_8_sig")
+    res = []
+    for i in data:
+        if i.endswith("\n"):
+            i = i.replace("\n", "").replace("\r", "")
+        d = json.loads(i)
+        temp_dict = {}
+        for item in columns:
+            temp_dict[item] = d.get(item, None)
+        # print(d.keys())
+        res.append(temp_dict)
+    return res
+
+
+def generateCsv(file):
+    file_new_name = file.split(".log")[0] + ".csv"
+    print(file_new_name)
+    csv_file = open(file_new_name, 'w', encoding="utf_8_sig", newline="")
+    json_list = readFile(file)
+    json_values = []
+    for one in json_list:
+        json_values.append(one.values())
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(columns)
+    csv_writer.writerows(json_values)
+    csv_file.close()
+
+
+if __name__ == '__main__':
+    all_file = eachFiles(r"D:\桌面\微信top200")
+    for item in all_file:
+        generateCsv(item)
